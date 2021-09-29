1786a144fde45cda812e73b52821542063b379f3
Riccardo Maggioni <riccardo.maggioni@googlemail.com>
2021-09-29 18:25:48 +0200

    directory app

--- CameraCal/.ipynb_checkpoints/CamerCal-checkpoint.py
+++ CameraCal/.ipynb_checkpoints/CamerCal-checkpoint.py
@@ -20,6 +20,8 @@ class ChessboardApp:
         self.endEvent = None
         self.chessboards = []
         self.n = 0
+        #self.ctrl serve per attivare il delay dopo un'immagine buona in modo che l'utente capisca che è stata presa
+        self.ctrl = False
         
         
         #create the main window of an application
@@ -27,14 +29,17 @@ class ChessboardApp:
         #panel for image
         self.panel = None
         
+        #barra che indica il progresso nel raccoglimento foto
         pbar = ttk.Progressbar(self.root, orient = 'horizontal', length = 300, mode = 'determinate')
         pbar['value'] = self.n * 100/14
         pbar.pack(padx=10, pady = 10)
         
+        #testo che indica quante foto raccogliere e quante da raccogliere
         value_label = ttk.Label(self.root, text = f"Current Progress: {self.n}/14")
         value_label.pack(padx=10, pady =10)
         
         if self.n >= 14:
+            #se è stato raggiunto il numero giusto di foto il bottone è cliccabile e azionerà la creazione e salvataggio della matrice
             #per creare un bottone:
             btn = tk.Button(self.root, text = "SAVE", command = self.SaveMatrix, bg = "#07e041")
             #pack è uno dei geometry managment mechanism
@@ -64,10 +69,14 @@ class ChessboardApp:
                 
                 #analize image for chessboard
                 if self.n<14:
+                    #attiva l'analisi delle foto finchè necessario
                     chessboard = Chessboard(nx = 7, ny = 6,frame = self.frame)
                     if chessboard.ret == True:
+                        self.ctrl = True
                         self.chessboards.append(chessboard)
                         self.n += 1
+                        cv2.drawChessboardCorners(self.frame, (7,6), chessboard.corners2, chessboard.ret)
+                        
                     
                 #opencv represents image in BGR, we need to convert i RGB
                 image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
@@ -75,6 +84,7 @@ class ChessboardApp:
                 image = ImageTk.PhotoImage(image)
                 
                 if self.panel is None:
+                    #blocco display del video
                     self.panel = tk.Label(image=image)
                     self.panel.image = image
                     self.panel.pack(side="top", padx=10, pady=10)
@@ -82,7 +92,12 @@ class ChessboardApp:
                 else:
                     self.panel.configure(image=image)
                     self.panel.image = image
-        
+                
+                if self.ctrl:
+                    
+                    time.sleep(1)
+                    self.ctrl = False
+                    
         except RuntimeError:
             print("[INFO] caught a Runtimeerror")
     
@@ -92,7 +107,7 @@ class ChessboardApp:
         objpoints = []
         imgpoints = []
         shape = self.chessboards[0].dimensions
-        for chessboard in chessboards:
+        for chessboard in self.chessboards:
             objpoints.append(chessboard.objpoints)
             imgpoints.append(chessboard.imgpoints)
             
