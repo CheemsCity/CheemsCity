dfce862302e02a4aba22414662c625f6760f45e7
rickymaggio02 <riccardo.maggioni@googlemail.com>
2021-11-14 19:26:26 +0100

    sistemato libreria motori

--- Robot/Motor.py
+++ Robot/Motor.py
@@ -8,7 +8,7 @@ class Motor:
     
     def Power(self, motor, power):
         #motor è un char che indica se motore destro(r) o sinistro(l)
-        #power è un int compreso tra 0 e 100, min e max
+        #power è un float compreso tra -100 e 100, con i numeri negativi a significare la direzione contraria.
         try:
             msg = "s" + "m" + motor + str(power)
             self.comm.SendCommand(msg)
@@ -45,8 +45,29 @@ class Motor:
                 print("problema comunicazione motore destro")
         time.sleep(1)
     
-    def Avanti(self, meter)
+    def Avanti(self, power):
+        ret=self.Power('r',power)
+        if(!ret):
+            print("problema motore destro")
+        ret = self.Power('l', power)
+        if(!ret):
+            print("problema motore sinistro")
+
     
-    def Indietro(self, meter)
-    
-    def Gira(self, angle)
+    def Indietro(self, power):
+        ret = self.Power('r', power*(-1))
+        if(!ret):
+            print("problema motore destro")
+        ret = self.Power('l', power * (-1))
+        if(!ret):
+            print("problema motore sinistro")
+
+    def Left(self, power):
+        ret = self.Power('r', power)
+        if(!ret):
+            print("problema motore destro")
+
+    def Right(self, power):
+        ret = self.Power('l', power)
+        if(!ret):
+            print("problema motore sinistro")
---
+++ Robot/pipeline.py
@@ -0,0 +1,46 @@
+import cv2
+from picamera import PiCamera
+from CameraStream import CameraStream
+from threading import Thread
+import time
+import numpy as np
+import os
+from Curves import curves
+from LaneFilter import LaneFilter 
+from BirdView import BirdView
+
+vs = CameraStream().start()
+time.sleep(2.0)
+
+import yaml
+with open(r'FinalCalibration.yml') as file:
+# The FullLoader parameter handles the conversion from YAML
+# scalar values to Python the dictionary format
+    calibration_data = yaml.load(file, Loader=yaml.UnsafeLoader)
+    matrix = calibration_data['camera_matrix']
+    dist_coef = calibration_data['distortion_coefficient']
+
+lanefilter = LaneFilter()
+
+matrix = calibration_data['camera_matrix']
+dist_coef = calibration_data['distortion_coefficient']
+
+height = 240
+source_points = [(0, height), (300, height), (250,150), (50, 150)]
+dest_points = [(100, height), (220, height), (220, 0), (100, 0)]
+
+birdview = BirdView(source_points, dest_points, matrix, dist_coef)
+curves = curves(9, 20, 50)
+
+while True:
+    image = vs.read()
+    lane_image = np.copy(image)
+    filtered = lanefilter.toCanny(lane_image)
+    filtered = lanefilter.ROI(filtered)
+    skyview = birdview.sky_view(filtered)
+    undistort = birdview.undistort(image)
+    result = curves.Detect(skyview,0)
+    combo = birdview.Visual(image, skyview, result['pixel_left_best_fit_curve'], result['pixel_right_best_fit_curve'])
+    cv2.imshow("frame", combo)
+    key = cv2.waitKey(1) & 0xFF
+    
