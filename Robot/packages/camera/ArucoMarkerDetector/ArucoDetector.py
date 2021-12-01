3fd78762b092f4de950dcd5683144ef7d999e7a2
ManneredPizza <peroni.jacopo@gmail.com>
2021-12-01 01:28:51 +0100

    aggiunta della box intorno al marker, del sistema di rifermento centrato nel marker ed ortogonale ad esso, nuova calimbrazione su posizione definitiva della camera

--- Robot/packages/camera/ArucoMarkerDetector/ArucoDetector.py
+++ Robot/packages/camera/ArucoMarkerDetector/ArucoDetector.py
@@ -1,6 +1,6 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 class ArucoDetector:
 
@@ -14,13 +14,16 @@ class ArucoDetector:
         self.corners = None
         self.ids = None
         self.rejected = None
+        self.rvec = None
+        self.tvec = None
+        self._ = None
         #lunghezza di un lato di un marker nelle dimensioni reali in metri
-        self.markerLength = 0.06
+        self.markerLength = 0.03
 
     def undistort(self, raw_image):
         #annullare gli effetti di curvatura della camera
-        gray = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
-        image = cv2.undistort(gray, self.cam_matrix, self.dist_coeff, None, self.cam_matrix)
+        #gray = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+        image = cv2.undistort(raw_image, self.cam_matrix, self.dist_coeff, None, self.cam_matrix)
         return image
 
     def frameDetector(self, image):
@@ -35,4 +38,34 @@ class ArucoDetector:
     def estimatePoseMarkers(self):
         #questa funzione mi restituisce le cooridnate del centro del marker nel sistema di rifermineto centrato nella camera
         #le coordinate dei vertici del marker sono (-markerLength/2, markerLength/2, 0), (markerLength/2, markerLength/2, 0), (markerLength/2, -markerLength/2, 0), (-markerLength/2, -markerLength/2, 0)
-        return cv2.aruco.estimatePoseSingleMarkers(self.corners, self.markerLength, self.cam_matrix, self.dist_coeff)
+        self.rvec, self.tvec, self._ = cv2.aruco.estimatePoseSingleMarkers(self.corners, self.markerLength, self.cam_matrix, self.dist_coeff)
+        return self.rvec, self.tvec, self._
+
+    def rodrigues(self, r):
+        #applico la formula di rodrigues per la trasformazione di un vettore rotazione in una matrice di rotazione
+        #https://courses.cs.duke.edu/fall13/compsci527/notes/rodrigues.pdf
+        #pag 5
+        theta = np.linalg.norm(r)
+        u = np.array(r / theta)
+        cross_u = np.array([[0,-u[2],u[1]],[u[2],0,-u[0]],[-u[1],u[0],0]])
+        R = np.array(np.eye(3)*math.cos(theta) + (1-math.cos(theta))*np.matmul(u,u.T) + cross_u*math.sin(theta))
+        return R
+
+    def drawDetectedMarkers(self, image):
+        #disegna un rettangolo attorno ai marker, a mano questo codice disegna i sementi tra i punti definiti in corners
+        return cv2.aruco.drawDetectedMarkers(image, self.corners)
+
+    def drawAxis(self, image):
+        #disegna i sistemi di riferimento centrati nel cetro dei vari marker, a mano rvec mi da la distanza dalla camera al centro dei marker
+        #tvec mi da una rotazione del marker e markerLegnth mi da la lunghezza vera del lato del marker
+        #questi dati mi permettono di definire le coordinate 3D del centro e dei corner, a quel punto basta definire il piano passante
+        #per i 4 punti e si ha il sistema di riferimento
+        for i in range(len(self.ids)):
+            image = cv2.aruco.drawAxis(image, self.cam_matrix, self.dist_coeff, self.rvec[i], self.tvec[i], self.markerLength/2)
+        return image
+
+    def printImage(self, image):
+        comboBig = cv2.resize(image, (640,480))
+        cv2.imshow("frame", comboBig)
+        key = cv2.waitKey(1) & 0xFF
+        #TODO il programma non ha una safe exit, l'unico modo per chiuderlo è usare killall python, un po' brutale
--- Robot/packages/camera/ArucoMarkerDetector/detector.py
+++ Robot/packages/camera/ArucoMarkerDetector/detector.py
@@ -1,10 +1,8 @@
 from camera.ArucoMarkerDetector.ArucoDetector import ArucoDetector
 from camera.CameraStream import CameraStream
 import time
-import cv2
 import yaml
-from threading import Thread
-import matplotlib.pyplot as plt
+import numpy as np
 
 with open(r'FinalCalibration.yml') as file:
     calibration_data = yaml.load(file, Loader=yaml.UnsafeLoader)
@@ -14,24 +12,19 @@ with open(r'FinalCalibration.yml') as file:
 vs = CameraStream().start()
 time.sleep(2.0)
 
-#img = cv2.imread('prova.jpg', 0) 
-
 detector = ArucoDetector(cam_matrix, dist_coeff)
 
-#while True:
-raw_image = vs.read()
-image = detector.undistort(raw_image)
-corner = detector.frameDetector(image)
-rvec, tvec, _ = detector.estimatePoseMarkers()
-print(rvec[0][0])
-#print(tvecs)
-for i in range(1):
-    for j in range(4):
-        plt.plot(corner[i][0][j][0],corner[i][0][j][1],marker='v', color="white")
-plt.imshow(image)
-plt.show()
-#comboBig = cv2.resize(image, (640,480))
-#cv2.imshow("frame", comboBig)
-#key = cv2.waitKey(1) & 0xFF
-
-    
+while True:
+    raw_image = vs.read()
+    #correggo la curvatura derivante dalla camera
+    image = detector.undistort(raw_image)
+    #calcolo le coordinate dei vertici dei quadrati
+    corner = detector.frameDetector(image)
+    #calcolo il vettore distanza dalla camera e la rotazione rispetto ad essa per ogni aruco marker
+    rvec, tvec, _ = detector.estimatePoseMarkers()
+    #stampo sull'immagine i rettangoli intorno ai marker
+    image = detector.drawDetectedMarkers(image)
+    #stampo sull'immagine gli assi centrati
+    image = detector.drawAxis(image)
+    #TODO quando non ci sono marker nell'immagine va in errore per il len(self.ids), inventarsi un try catch.
+    detector.printImage(image)
