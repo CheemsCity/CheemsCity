f18a207716b85fe64e61f973e4081fcb4ab73e16
rickymaggio02 <riccardo.maggioni@googlemail.com>
2021-12-25 22:38:19 +0100

    spostamento della dimensione del frame camera in cameraConfig.yaml e modifica programmi che ne facevano uso

--- Robot/packages/camera/LineDetector/LaneFilter.py
+++ Robot/packages/camera/LineDetector/LaneFilter.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
+import yaml
 
 class LaneFilter:
     
     def __init__(self):
         self.problem = 0
+        with open(r'../cameraConfig.yaml') as file:
+            self.settings = yaml.full_load(file)
+            self.height = self.settings['res_h']
+            self.width = self.settings['res_w']
         
     def toCanny(self,img):
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -13,11 +18,9 @@ class LaneFilter:
         return canny
     
     def ROI(self,img):
-        height = img.shape[0]
-        width = img.shape[1]
         #roy da modificare quando avremo la struttura della macchina e quindi posizione fissa della telecamera
         trapezio = np.array([
-            [(0, height), (width, height), (width, 150), (0,150)]
+            [(0, self.height), (self.width, self.height), (self.width, 150), (0,150)]
         ])
         mask = np.zeros_like(img)
         cv2.fillPoly(mask, trapezio, 255) #crea maschera
