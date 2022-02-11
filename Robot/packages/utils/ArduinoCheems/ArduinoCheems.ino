838e2e3bbc175f022582a577d7c5c11e758cc19e
Riccardo Maggioni <riccardo.maggioni@googlemail.com>
2022-02-11 12:06:47 +0100

    Update Arduino
    Modifica per fare in modo che i motori rispettino il movimento previsto del robot

--- Robot/packages/utils/ArduinoCheems/ArduinoCheems.ino
+++ Robot/packages/utils/ArduinoCheems/ArduinoCheems.ino
@@ -82,7 +82,7 @@ void setPower(int power, int IN1_PIN, int IN2_PIN){
 
 void setMotorPower(int power, char motor){
 	if(motor == 'r'){
-		setPower(power, MA1, MA2);
+		setPower(-(power), MA1, MA2);
 	}
 	else if(motor == 'l'){
 		setPower(power, MB1, MB2);
