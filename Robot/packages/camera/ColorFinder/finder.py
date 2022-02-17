3ba3b7808425e134a320477190d16317dd3259bf
Jacopo Peroni <jacopoperoni@MBP-di-Jacopo.homenet.telecomitalia.it>
2022-02-17 16:49:20 +0100

    creazione rettangolo da evitare

--- Robot/packages/camera/ColorFinder/finder.py
+++ Robot/packages/camera/ColorFinder/finder.py
@@ -10,6 +10,7 @@ class ColorFinder():
         self.raggio = radius #"raggio" di'incertezza, per info vedasi documentazione
         self.width = self.image.shape[0] #larghezza immagine
         self.height = self.image.shape[1] #altezza immagine
+        self.risk_coeff = 0.1 #coefficiente di rischio per tenere la distanza dall'oggetto trovato
 
     def mat_diff(self):
         Mcolor = np.ones((self.width,self.height,3))
@@ -26,6 +27,7 @@ class ColorFinder():
         self.Md = np.sqrt(self.Mdiff[:,:,0] + self.Mdiff[:,:,1] + self.Mdiff[:,:,2])
         #sommo gli elementi al quadrato e ne prendo la radice, così ottengo la distanza euclidea tra i vettori di dimensione 3
         #della profondità dell'immagine con il vettore colore
+        return self.Md
 
     #l'idea usata nel codice di cui sopra è essenzialmente questa:
     #voglio prendere il vettore image[i,j] fatto da RGB del pixel (i,j)
@@ -48,26 +50,55 @@ class ColorFinder():
                     self.bool_Md[i,j] = 0
                 else:
                     self.bool_Md[i,j] = 255
+        return self.bool_Md
 
     def CannyContour(self):
         self.contour_Md = cv2.Canny(self.bool_Md, 70, 150)
         #trovo i contorni tramite l'algoritmo Canny sfruttando il fatto che ove era nel raggio
         #ora c'è il nero e nel resto c'è il bianco
+        return self.contour_Md
 
-    def ColorContour(self):
+    def ColorContour(self, draw):
         self.contours, self.hierarchy = cv2.findContours(self.contour_Md,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         #trovo i contorni dell'immagine tramite l'algoritmo di OpenCv
-        cv2.drawContours(self.image, self.contours, -1, (0, 255, 0), 3)
+        if draw==1:
+            cv2.drawContours(self.image, self.contours, -1, (0, 255, 0), 3)
         #li disegno nell'immagine
+        return self.image
 
     def findBiggest(self):
-        length_list = list(filter(lambda i: len(self.contours[i]), range(len(self.contours))))
+        length_list = list(len(self.contours[i]) for i in range(len(self.contours)))
         #creo una lista con tutte le lunghezze dei vari contorni
         max_item = max(length_list)
         #prendo il contorno maggiore 
-        self.biggest_list = [index for index in range(len(length_list)) if length_list[index] == max_item]
+        self.biggest_list = list(filter(lambda i: len(self.contours[i]) >= max_item, range(len(self.contours))))
         #prendo gli indici degli elementi che hanno quel valore
+        return self.biggest_list
 
     def findBiggerThan(self,limit):
         self.result_bigger_list = list(filter(lambda i: len(self.contours[i]) > limit, range(len(self.contours))))
         #prendo gli indici degli elementi che hanno lunghezza maggiore di un limite
+        return self.result_bigger_list
+
+    def defineRectangularContour(self,index):
+        column = []
+        row = []
+        l = self.contours[index]
+        #prendo la lista dei punti che costituiscono il contorno più grande in foto
+        for i in range(len(l)):
+            row.append(l[i][0][0])
+            column.append(l[i][0][1])
+        #creo la lista delle colonne e la lista delle righe
+        self.SO = (math.ceil(max(row)*(1+self.risk_coeff)),math.ceil(min(column)*(1-self.risk_coeff)))
+        self.SE = (math.ceil(max(row)*(1+self.risk_coeff)),math.ceil(max(column)*(1+self.risk_coeff)))
+        self.NE = (math.ceil(min(row)*(1-self.risk_coeff)),math.ceil(max(column)*(1+self.risk_coeff)))
+        self.NO = (math.ceil(min(row)*(1-self.risk_coeff)),math.ceil(min(column)*(1-self.risk_coeff)))
+        #prendo i 4 punti che costruiscono il rettangolo più grande che contiene l'oggetto
+        #inflaziono il rettangolo aumentando i massimi del coefficiente di rischio
+        #e diminuendo i minimi dello sesso coefficiente
+
+        return[self.SO, self.SE, self.NE, self.NO]
+
+    def rectangleOnOriginalImage(self):
+        return cv2.rectangle(self.or_image, self.SO, self.NE, (255,0,0), 2)
+        #restituisco il rettangolo calcolato in sovraimpressione all'immagine originale
