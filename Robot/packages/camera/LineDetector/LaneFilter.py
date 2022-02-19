aa3f9332617bbd4585d3ae51466423e9353b3ac0
rickymaggio02 <riccardo.maggioni@googlemail.com>
2022-02-19 12:51:55 +0100

    modifiche punti del birdview

--- Robot/packages/camera/LineDetector/LaneFilter.py
+++ Robot/packages/camera/LineDetector/LaneFilter.py
@@ -16,13 +16,13 @@ class LaneFilter:
     def toCanny(self,img):
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         blur = cv2.GaussianBlur(gray, (5,5),0)
-        canny = cv2.Canny(blur, 70, 150)
+        canny = cv2.Canny(blur, 70, 170)
         return canny
     
     def ROI(self,img):
         #roy da modificare quando avremo la struttura della macchina e quindi posizione fissa della telecamera
         trapezio = np.array([
-            [(0, self.height), (self.width, self.height), (self.width, 150), (0,150)]
+            [(0, self.height), (self.width, self.height), (self.width, 200), (0,200)]
         ])
         mask = np.zeros_like(img)
         cv2.fillPoly(mask, trapezio, 255) #crea maschera
--- Robot/packages/camera/LineDetector/pipeline.py
+++ Robot/packages/camera/LineDetector/pipeline.py
@@ -6,13 +6,14 @@ import yaml
 import cv2
 import os
 from pkg_resources import resource_string
+import matplotlib.pyplot as plt
 
 class LineDetectorPipeline:
     def __init__(self):
         self.lanefilter = LaneFilter()
-        self.height = 240
-        self.source_points = [(0, self.height), (300, self.height), (250,150), (50, 150)]
-        self.dest_points = [(100, self.height), (220, self.height), (220, 0), (100, 0)]
+        self.height = 479
+        self.source_points = [(0, self.height), (600, self.height), (360,240), (230, 240)]
+        self.dest_points = [(130, self.height), (500, self.height), (500, 0), (130, 0)]
 
         #with open(r'/home/pi/CheemsCity/Robot/packages/camera/FinalCalibration.yml') as file:
         file = resource_string('camera', 'FinalCalibration.yml')
@@ -45,7 +46,7 @@ class LineDetectorPipeline:
         lane_image = np.copy(image)
         filtered = self.lanefilter.toCanny(lane_image)
         filtered = self.lanefilter.ROI(filtered)
-        res['ROI'] = np.copy(filtered)
+        roi = np.copy(filtered)
         skyview = self.birdview.sky_view(filtered)
         result = self.curves.Detect(skyview,0) #molto lento, va ottimizzato
         if result != -1:
@@ -54,7 +55,11 @@ class LineDetectorPipeline:
         else:
             self.comboBig = cv2.resize(lane_image, self.settings['line_detector_resizeImage'])
 
-        res['combo'] = self.comboBig
+        res = {
+            'roi' : roi,
+            'combo' : self.comboBig,
+            'skyview': skyview
+        }
         return res
     
 if __name__ == '__main__':
@@ -68,10 +73,15 @@ if __name__ == '__main__':
     white_flag = True
     while white_flag:
         image = vs.read()
-        res = detector.viewAll(self, image)
-        #comboBig = detector.lineDetector(image)
-        cv2.imshow("frame", res['combo'])
-        cv2.imshow("roi", res['ROI'])
+        #res = detector.viewAll( image)
+        comboBig = detector.lineDetector(image)
+        #cv2.imshow("frame", res['combo'])
+        cv2.imshow("frame", comboBig)
+        #cv2.imshow("roi", res['roi'])
+        #cv2.imshow("skyview", res['skyview'])
+        #plt.imshow(res['roi'])
+        #plt.imshow(res['skyview'])
+        #plt.show()
         key = cv2.waitKey(1) & 0xFF
         if key == 27:         #ESC
             white_flag = False
